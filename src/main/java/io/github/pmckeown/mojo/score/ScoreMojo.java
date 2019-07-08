e7e3f47aa8e773b0a25a93f09003659ae4d692e9
Paul McKeown <pmckeown@gmail.com>
2019-07-08 18:55:59 +1200

    Added maximum allowed inherited risk score and build now fails on a score greater that the max (#14)

--- src/main/java/io/github/pmckeown/mojo/score/ScoreMojo.java
+++ src/main/java/io/github/pmckeown/mojo/score/ScoreMojo.java
@@ -4,7 +4,6 @@ import io.github.pmckeown.mojo.AbstractDependencyTrackMojo;
 import io.github.pmckeown.rest.model.GetProjectsResponse;
 import io.github.pmckeown.rest.model.Metrics;
 import io.github.pmckeown.rest.model.Project;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -29,8 +28,11 @@ public class ScoreMojo extends AbstractDependencyTrackMojo {
     @Parameter(defaultValue = "${project}")
     private MavenProject mavenProject;
 
+    @Parameter
+    private Integer inheritedRiskScoreThreshold;
+
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoFailureException {
         info("score goal started");
 
         GetProjectsResponse response = dependencyTrackClient().getProjects();
@@ -48,23 +50,40 @@ public class ScoreMojo extends AbstractDependencyTrackMojo {
             if (projectOptional.isPresent()) {
                 Project project = projectOptional.get();
                 Metrics metrics = project.getMetrics();
+                int inheritedRiskScore = metrics.getInheritedRiskScore();
 
                 info(DELIMITER);
                 info("Project: %s, Version: %s", project.getName(), project.getVersion());
+                StringBuffer sb = new StringBuffer(format("Inherited Risk Score: %d", inheritedRiskScore));
 
-                String message = format("Inherited Risk Score: %d", metrics.getInheritedRiskScore());
-                if (metrics.getInheritedRiskScore() > 0) {
-                    warning(message);
+                if (inheritedRiskScoreThreshold != null) {
+                    sb.append(format(" - Maximum allowed Inherited Risk Score: %d", inheritedRiskScoreThreshold));
+                }
+
+                if (inheritedRiskScore > 0) {
+                    warning(sb.toString());
                 } else {
-                    info(message);
+                    info(sb.toString());
                 }
                 info(DELIMITER);
+
+                compareScoreToThreshold(inheritedRiskScore);
 
             } else {
                 error("Failed to find project on server: Project: %s, Version: %s");
             }
         } else {
             error("Failed to get projects from Dependency Track: " + response.getStatusText());
+        }
+    }
+
+    private void compareScoreToThreshold(int inheritedRiskScore) throws MojoFailureException {
+        debug("Inherited Risk Score Threshold set to: ", inheritedRiskScoreThreshold);
+
+        if (inheritedRiskScoreThreshold != null && inheritedRiskScore > inheritedRiskScoreThreshold) {
+
+            throw new MojoFailureException(format("Inherited Risk Score [%d] was greater than the " +
+                    "configured threshold [%d]", inheritedRiskScore, inheritedRiskScoreThreshold));
         }
     }
 
@@ -82,5 +101,9 @@ public class ScoreMojo extends AbstractDependencyTrackMojo {
      */
     void setMavenProject(MavenProject mavenProject) {
         this.mavenProject = mavenProject;
+    }
+
+    void setInheritedRiskScoreThreshold(int threshold) {
+        this.inheritedRiskScoreThreshold = threshold;
     }
 }
