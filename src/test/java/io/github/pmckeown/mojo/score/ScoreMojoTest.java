334008491aa7d11600fd859064f489ea4003c97d
Paul McKeown <pmckeown@gmail.com>
2019-07-08 18:33:26 +1200

    Added maximum allowed inherited risk score and build now fails on a score greater that the max

--- src/test/java/io/github/pmckeown/mojo/score/ScoreMojoTest.java
+++ src/test/java/io/github/pmckeown/mojo/score/ScoreMojoTest.java
@@ -2,6 +2,7 @@ package io.github.pmckeown.mojo.score;
 
 import io.github.pmckeown.mojo.AbstractDependencyTrackMojoTest;
 import io.github.pmckeown.rest.ResourceConstants;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +10,8 @@ import org.junit.Test;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.github.pmckeown.TestMojoLoader.loadScoreMojo;
 import static io.github.pmckeown.rest.ResourceConstants.V1_PROJECT;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 public class ScoreMojoTest extends AbstractDependencyTrackMojoTest {
 
@@ -31,5 +34,60 @@ public class ScoreMojoTest extends AbstractDependencyTrackMojoTest {
         scoreMojo.execute();
 
         verify(exactly(1), getRequestedFor(urlEqualTo(V1_PROJECT)));
+    }
+
+    @Test
+    public void thatARiskScoreHigherThanTheThresholdCausesBuildToFail() throws Exception {
+        // The current project score in the JSON file is 3
+        stubFor(get(urlEqualTo(ResourceConstants.V1_PROJECT)).willReturn(
+                aResponse().withBodyFile("api/v1/project/get-all-projects.json")));
+
+        ScoreMojo scoreMojo = loadScoreMojo(mojoRule);
+        scoreMojo.setDependencyTrackBaseUrl("http://localhost:" + wireMockRule.port());
+        scoreMojo.setMavenProject(mavenProject);
+        scoreMojo.setInheritedRiskScoreThreshold(1);
+
+        try {
+            scoreMojo.execute();
+            fail("Exception expected");
+        } catch (MojoFailureException ex) {
+            assertNotNull(ex);
+        }
+    }
+
+    @Test
+    public void thatARiskScoreEqualToTheThresholdDoesNothing() throws Exception {
+        // The current project score in the JSON file is 3
+        stubFor(get(urlEqualTo(ResourceConstants.V1_PROJECT)).willReturn(
+                aResponse().withBodyFile("api/v1/project/get-all-projects.json")));
+
+        ScoreMojo scoreMojo = loadScoreMojo(mojoRule);
+        scoreMojo.setDependencyTrackBaseUrl("http://localhost:" + wireMockRule.port());
+        scoreMojo.setMavenProject(mavenProject);
+        scoreMojo.setInheritedRiskScoreThreshold(3);
+
+        try {
+            scoreMojo.execute();
+        } catch (MojoFailureException ex) {
+            fail("Exception not expected");
+        }
+    }
+
+    @Test
+    public void thatARiskScoreLowerThanTheThresholdDoesNothing() throws Exception {
+        // The current project score in the JSON file is 3
+        stubFor(get(urlEqualTo(ResourceConstants.V1_PROJECT)).willReturn(
+                aResponse().withBodyFile("api/v1/project/get-all-projects.json")));
+
+        ScoreMojo scoreMojo = loadScoreMojo(mojoRule);
+        scoreMojo.setDependencyTrackBaseUrl("http://localhost:" + wireMockRule.port());
+        scoreMojo.setMavenProject(mavenProject);
+        scoreMojo.setInheritedRiskScoreThreshold(999);
+
+        try {
+            scoreMojo.execute();
+        } catch (MojoFailureException ex) {
+            fail("Exception not expected");
+        }
     }
 }
