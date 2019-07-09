f0fb0de2d212e72c74a6f28c64acd273319bb76a
Paul McKeown <pmckeown@gmail.com>
2019-07-09 21:47:00 +1200

    Tidied up Unirest common config

--- src/main/java/io/github/pmckeown/rest/client/DependencyTrackClient.java
+++ src/main/java/io/github/pmckeown/rest/client/DependencyTrackClient.java
@@ -1,7 +1,10 @@
 package io.github.pmckeown.rest.client;
 
 import io.github.pmckeown.rest.model.*;
-import kong.unirest.*;
+import kong.unirest.GenericType;
+import kong.unirest.HttpResponse;
+import kong.unirest.JacksonObjectMapper;
+import kong.unirest.Unirest;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
@@ -18,24 +21,24 @@ import static kong.unirest.HeaderNames.*;
  */
 public class DependencyTrackClient {
 
-    private static final int CLIENT_EXCEPTION_STATUS = -1;
     private String host;
     private String apiKey;
+
+    static {
+        Unirest.config().setObjectMapper(new JacksonObjectMapper(relaxedObjectMapper()))
+                .addDefaultHeader(ACCEPT_ENCODING, "gzip, deflate")
+                .addDefaultHeader(ACCEPT, "application/json");
+    }
 
     public DependencyTrackClient(String host, String apiKey) {
         this.host = normaliseHost(host);
         this.apiKey = apiKey;
-
-        // TODO - move to class context
-        Unirest.config().setObjectMapper(new JacksonObjectMapper(relaxedObjectMapper()));
     }
     
     public Response uploadBom(Bom bom) {
         HttpResponse<String> httpResponse = Unirest.put(host + V1_BOM)
                 .header(CONTENT_TYPE, "application/json")
                 .header("X-Api-Key", apiKey)
-                .header(ACCEPT_ENCODING, "gzip, deflate")
-                .header(ACCEPT, "application/json")
                 .body(bom)
                 .asString();
 
@@ -53,8 +56,6 @@ public class DependencyTrackClient {
     public ResponseWithOptionalBody<List<Project>> getProjects() {
         HttpResponse<List<Project>> httpResponse = Unirest.get(host + V1_PROJECT)
                 .header("X-Api-Key", apiKey)
-                .header(ACCEPT_ENCODING, "gzip, deflate")
-                .header(ACCEPT, "application/json")
                 .asObject(new GenericType<List<Project>>(){});
 
         Optional<List<Project>> body;
@@ -71,8 +72,6 @@ public class DependencyTrackClient {
     public ResponseWithOptionalBody<Metrics> getMetrics(String projectUuid) {
         final HttpResponse<Metrics> httpResponse = Unirest.get(host + V1_CURRENT_PROJECT_METRICS)
                 .header("X-Api-Key", apiKey)
-                .header(ACCEPT_ENCODING, "gzip, deflate")
-                .header(ACCEPT, "application/json")
                 .routeParam("uuid", projectUuid)
                 .asObject(new GenericType<Metrics>(){});
 
