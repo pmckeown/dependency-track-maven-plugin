d0eecd886c86de70f26c7314f5f8d3a24e27f9e8
Paul McKeown <pmckeown@gmail.com>
2019-09-29 18:58:32 +1300

    Added test for finding sorter

--- src/test/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportTest.java
+++ src/test/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportTest.java
@@ -2,6 +2,7 @@ package io.github.pmckeown.dependencytrack.finding.report;
 
 import io.github.pmckeown.dependencytrack.finding.Finding;
 import io.github.pmckeown.dependencytrack.finding.FindingThresholds;
+import io.github.pmckeown.dependencytrack.finding.Severity;
 import org.junit.Test;
 
 import java.util.List;
@@ -32,6 +33,35 @@ public class FindingsReportTest {
         assertThat(findingsReport.getCritical().getCount(), is(equalTo(1)));
         assertThat(findingsReport.getCritical().getFindings().get(0).getComponent().getName(),
                 is(equalTo("shonky-lib")));
+    }
+
+    @Test
+    public void thatAFindingsAreSortedIntoSeparateBuckets() {
+        FindingThresholds findingThresholds = new FindingThresholds(1, null, null, null);
+        List<Finding> findings = aListOfFindings()
+                .withFinding(aFinding()
+                        .withAnalysis(anAnalysis())
+                        .withVulnerability(aVulnerability().withSeverity(Severity.CRITICAL))
+                        .withComponent(aComponent()))
+                .withFinding(aFinding()
+                        .withAnalysis(anAnalysis())
+                        .withVulnerability(aVulnerability().withSeverity(Severity.HIGH))
+                        .withComponent(aComponent()))
+                .withFinding(aFinding()
+                        .withAnalysis(anAnalysis())
+                        .withVulnerability(aVulnerability().withSeverity(Severity.MEDIUM))
+                        .withComponent(aComponent()))
+                .withFinding(aFinding()
+                        .withAnalysis(anAnalysis())
+                        .withVulnerability(aVulnerability().withSeverity(Severity.LOW))
+                        .withComponent(aComponent()))
+                .build();
+        FindingsReport findingsReport = new FindingsReport(findingThresholds, findings);
+
+        assertThat(findingsReport.getCritical().getCount(), is(equalTo(1)));
+        assertThat(findingsReport.getHigh().getCount(), is(equalTo(1)));
+        assertThat(findingsReport.getMedium().getCount(), is(equalTo(1)));
+        assertThat(findingsReport.getLow().getCount(), is(equalTo(1)));
     }
 
 }
