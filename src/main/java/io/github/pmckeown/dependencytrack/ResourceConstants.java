0d173790f63a0ffd3cd095747fd8941a0539e374
Rajees Patel <rajees.patel@anz.com>
2020-04-16 15:19:19 +1200

    Getting projects was limit to a limit of 100 projects, due to a default pagination limit of 100. Increase this to a arbitrary high number so we get all projects.

--- src/main/java/io/github/pmckeown/dependencytrack/ResourceConstants.java
+++ src/main/java/io/github/pmckeown/dependencytrack/ResourceConstants.java
@@ -9,7 +9,7 @@ public class ResourceConstants {
 
     public static final String V1_BOM = "/api/v1/bom";
     public static final String V1_BOM_TOKEN_UUID = "/api/v1/bom/token/{uuid}";
-    public static final String V1_PROJECT = "/api/v1/project";
+    public static final String V1_PROJECT = "/api/v1/project?limit=1000000&offset=0";
     public static final String V1_PROJECT_UUID = "/api/v1/project/{uuid}";
     public static final String V1_FINDING_PROJECT_UUID = "/api/v1/finding/project/{uuid}";
     public static final String V1_METRICS_PROJECT_UUID_CURRENT = "/api/v1/metrics/project/{uuid}/current";
