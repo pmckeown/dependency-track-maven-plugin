3b9621cc38da964e0db15956395df9cb27ab89c8
Michiel Hendriks <michiel.hendriks@ifv.nl>
2020-11-30 14:42:47 +0100

    another codacy adjustment

--- src/main/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportConstants.java
+++ src/main/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportConstants.java
@@ -6,11 +6,12 @@ class FindingsReportConstants {
         // Do no instantiate
     }
 
+    protected static final String XML_REPORT_FILENAME = "dependency-track-findings.xml";
+
+    protected static final String HTML_REPORT_FILENAME = "dependency-track-findings.html";
+
     static final String META_INF_DIRECTORY = "/META-INF";
 
     static final String XSL_STYLESHEET_FILENAME = META_INF_DIRECTORY + "/dependency-track-findings-transformer.xsl";
 
-    protected static final String XML_REPORT_FILENAME = "dependency-track-findings.xml";
-
-    protected static final String HTML_REPORT_FILENAME = "dependency-track-findings.html";
 }
