9bd1d6a3e3a23045e5b9ca6ae0fc7b62350a6c2f
Michiel Hendriks <michiel.hendriks@ifv.nl>
2020-11-30 14:17:59 +0100

    Removed unused import

--- src/test/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportGeneratorTest.java
+++ src/test/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportGeneratorTest.java
@@ -9,7 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.io.File;
 import java.util.List;
 
 import static io.github.pmckeown.dependencytrack.finding.FindingListBuilder.aListOfFindings;
