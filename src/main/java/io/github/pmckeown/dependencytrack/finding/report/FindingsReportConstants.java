25fe6264fced3c48d725cc15b00c355bbc1cd93f
Michiel Hendriks <michiel.hendriks@ifv.nl>
2020-11-30 14:56:35 +0100

    Moved constants even higher.

--- src/main/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportConstants.java
+++ src/main/java/io/github/pmckeown/dependencytrack/finding/report/FindingsReportConstants.java
@@ -2,13 +2,13 @@ package io.github.pmckeown.dependencytrack.finding.report;
 
 class FindingsReportConstants {
 
-    private FindingsReportConstants() {
-        // Do no instantiate
-    }
-
     protected static final String XML_REPORT_FILENAME = "dependency-track-findings.xml";
 
     protected static final String HTML_REPORT_FILENAME = "dependency-track-findings.html";
+
+    private FindingsReportConstants() {
+        // Do no instantiate
+    }
 
     static final String META_INF_DIRECTORY = "/META-INF";
 
