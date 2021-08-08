8e44ec160760c5baaa870082a22042dadf80e62c
Paul McKeown <pmckeown@gmail.com>
2021-08-08 19:03:51 +1200

    Made integration test work

--- src/test/java/io/github/pmckeown/dependencytrack/finding/FindingsMojoTest.java
+++ src/test/java/io/github/pmckeown/dependencytrack/finding/FindingsMojoTest.java
@@ -2,7 +2,9 @@ package io.github.pmckeown.dependencytrack.finding;
 
 import io.github.pmckeown.dependencytrack.CommonConfig;
 import io.github.pmckeown.dependencytrack.finding.report.FindingsReportGenerator;
+import io.github.pmckeown.dependencytrack.project.Project;
 import io.github.pmckeown.dependencytrack.project.ProjectAction;
+import io.github.pmckeown.dependencytrack.project.ProjectBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -11,6 +13,9 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -40,11 +45,11 @@ public class FindingsMojoTest {
 
     @Test
     public void thatReportIsAlwaysGenerated() throws Exception {
-//        doReturn(ProjectBuilder.aProject().build()).when(projectAction).getProject(anyString(), anyString());
+        doReturn(ProjectBuilder.aProject().build()).when(projectAction).getProject(anyString(), anyString());
         List<Finding> findings = FindingListBuilder.aListOfFindings().build();
-//        doReturn(findings).when(findingsAction).getFindings(any(Project.class));
-//        doReturn(false).when(findingsAnalyser).doNumberOfFindingsBreachPolicy(
-//                any(List.class), any(FindingThresholds.class));
+        doReturn(findings).when(findingsAction).getFindings(any(Project.class));
+        doReturn(false).when(findingsAnalyser).doNumberOfFindingsBreachPolicy(
+                any(List.class), any(FindingThresholds.class));
 
         findingsMojo.performAction();
 
