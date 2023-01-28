7017e27c74d5511d15dd4885c25ccd728cc23495
roadSurfer <38717523+roadSurfer@users.noreply.github.com>
2023-01-28 00:13:29 +0000

    Byte order mark (#344)
    * Handle possible byte-order mark in bom.xml
    
    * Adding missed unit test
    
    * Removed errant `final`
    
    * Removed errant `final`
    
    ---------
    
    Co-authored-by: jason <jason.irwin@idoxgroup.com>

--- src/test/java/io/github/pmckeown/dependencytrack/bom/BomParserTest.java
+++ src/test/java/io/github/pmckeown/dependencytrack/bom/BomParserTest.java
@@ -34,6 +34,17 @@ public class BomParserTest {
                 "SNAPSHOT?type=maven-plugin")));
         assertThat(info.getClassifier(), is(equalTo("LIBRARY")));
     }
+    @Test
+    public void thatProjectInfoCanBeParsedFromBomWithByteOrder() {
+        File bomFile = new File(BomParserTest.class.getResource("bom_byteorder.xml").getFile());
+        ProjectInfo info = bomParser.getProjectInfo(bomFile).get();
+        assertThat(info.getGroup(), is(equalTo("io.github.pmckeown")));
+        assertThat(info.getDescription(), is(equalTo("Maven plugin to integrate with a Dependency Track server to " +
+                "submit dependency manifests and gather project metrics.")));
+        assertThat(info.getPurl(), is(equalTo("pkg:maven/io.github.pmckeown/dependency-track-maven-plugin@1.2.1-" +
+                "SNAPSHOT?type=maven-plugin")));
+        assertThat(info.getClassifier(), is(equalTo("LIBRARY")));
+    }
 
     @Test
     public void thatEmptyIsReturnedWhenMissingBomFile() {
