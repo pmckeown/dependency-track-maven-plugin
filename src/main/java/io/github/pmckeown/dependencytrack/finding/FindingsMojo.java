57c55431156ca8454d20a2551922190eb52aa98d
Thomas Repnik <47736416+thomasrepnik@users.noreply.github.com>
2023-07-02 07:09:34 +0200

    added CLI compatibility for FindingsMojo (#381)
    Co-authored-by: Thomas Repnik <thomas.repnik@suva.ch>

--- src/main/java/io/github/pmckeown/dependencytrack/finding/FindingsMojo.java
+++ src/main/java/io/github/pmckeown/dependencytrack/finding/FindingsMojo.java
@@ -15,7 +15,6 @@ import org.apache.maven.project.MavenProject;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-
 import java.io.File;
 import java.util.List;
 
@@ -25,25 +24,25 @@ import static org.apache.maven.plugins.annotations.LifecyclePhase.VERIFY;
  * Print the findings retrieved from the Dependency Track Server after a BOM upload.  This is calculated immediately
  * by the server and as such can be used in situations where you want to know if a change to your application pom.xml
  * has had an impact on the vulnerabilities present in your application.
- *
+ * <p>
  * You can optionally define thresholds for failing the build where the number of issues in a particular category
  * is greater than the threshold you define for that category.
- *
+ * <p>
  * For example the following configuration with fail the build if there are any Critical or High issues found in the
  * scan, more than 10 medium issues or more than 20 low issues.
- *
+ * <p>
  * &lt;configuration&gt;
- *     &lt;findingThresholds&gt;
- *         &lt;critical&gt;0&lt;/critical&gt;
- *         &lt;high&gt;0&lt;/high&gt;
- *         &lt;medium&gt;10&lt;/medium&gt;
- *         &lt;low&gt;20&lt;/low&gt;
- *         &lt;unassigned&gt;30&lt;/unassigned&gt;
- *     &lt;/findingThresholds&gt;
+ * &lt;findingThresholds&gt;
+ * &lt;critical&gt;0&lt;/critical&gt;
+ * &lt;high&gt;0&lt;/high&gt;
+ * &lt;medium&gt;10&lt;/medium&gt;
+ * &lt;low&gt;20&lt;/low&gt;
+ * &lt;unassigned&gt;30&lt;/unassigned&gt;
+ * &lt;/findingThresholds&gt;
  * &lt;/configuration&gt;
- *
+ * <p>
  * This allows you to tune build failures to your risk appetite.
- *
+ * <p>
  * Specific configuration options are:
  * <ol>
  *     <li>findingThresholds</li>
@@ -67,19 +66,34 @@ public class FindingsMojo extends AbstractDependencyTrackMojo {
     @Parameter(name = "findingThresholds")
     private FindingThresholds findingThresholds;
 
+    @Parameter(property = "findingThresholds.critical")
+    private Integer thresholdCritical;
+
+    @Parameter(property = "findingThresholds.high")
+    private Integer thresholdHigh;
+
+    @Parameter(property = "findingThresholds.medium")
+    private Integer thresholdMedium;
+
+    @Parameter(property = "findingThresholds.low")
+    private Integer thresholdLow;
+
+    @Parameter(property = "findingThresholds.unassigned")
+    private Integer thresholdUnassigned;
+
     @Parameter(defaultValue = "${project}", readonly = true, required = false)
     private MavenProject mavenProject;
 
-    private ProjectAction projectAction;
-    private FindingsAction findingsAction;
-    private FindingsPrinter findingsPrinter;
-    private FindingsAnalyser findingsAnalyser;
-    private FindingsReportGenerator findingsReportGenerator;
+    private final ProjectAction projectAction;
+    private final FindingsAction findingsAction;
+    private final FindingsPrinter findingsPrinter;
+    private final FindingsAnalyser findingsAnalyser;
+    private final FindingsReportGenerator findingsReportGenerator;
 
     @Inject
-    public FindingsMojo(ProjectAction projectAction, FindingsAction findingsAction, FindingsPrinter findingsPrinter,
-                        FindingsAnalyser findingsAnalyser, FindingsReportGenerator findingsReportGenerator,
-                        CommonConfig commonConfig, Logger logger) {
+    public FindingsMojo(final ProjectAction projectAction, final FindingsAction findingsAction, final FindingsPrinter findingsPrinter,
+                        final FindingsAnalyser findingsAnalyser, final FindingsReportGenerator findingsReportGenerator,
+                        final CommonConfig commonConfig, final Logger logger) {
         super(commonConfig, logger);
         this.projectAction = projectAction;
         this.findingsAction = findingsAction;
@@ -90,34 +104,49 @@ public class FindingsMojo extends AbstractDependencyTrackMojo {
 
     @Override
     protected void performAction() throws MojoExecutionException, MojoFailureException {
-        List<Finding> findings;
+        final List<Finding> findings;
         try {
-            Project project = projectAction.getProject(commonConfig.getProjectName(), commonConfig.getProjectVersion());
+            final Project project = projectAction.getProject(commonConfig.getProjectName(), commonConfig.getProjectVersion());
             findings = findingsAction.getFindings(project);
             findingsPrinter.printFindings(project, findings);
-            boolean policyBreached = findingsAnalyser.doNumberOfFindingsBreachPolicy(findings, findingThresholds);
-            findingsReportGenerator.generate(getOutputDirectory(), findings, findingThresholds, policyBreached);
+            final FindingThresholds threshold = createThreshold();
+            final boolean policyBreached = findingsAnalyser.doNumberOfFindingsBreachPolicy(findings, threshold);
+            findingsReportGenerator.generate(getOutputDirectory(), findings, threshold, policyBreached);
 
             if (policyBreached) {
                 throw new MojoFailureException("Number of findings exceeded defined thresholds");
             }
-        } catch (DependencyTrackException ex) {
+        } catch (final DependencyTrackException ex) {
             handleFailure("Error occurred when getting findings", ex);
         }
+    }
+
+    FindingThresholds createThreshold() {
+        FindingThresholds thresholds = this.findingThresholds;
+
+        if (thresholds == null &&
+                (this.thresholdCritical != null
+                        || this.thresholdHigh != null
+                        || this.thresholdMedium != null
+                        || this.thresholdLow != null
+                        || this.thresholdUnassigned != null)) {
+            thresholds = new FindingThresholds(this.thresholdCritical, this.thresholdHigh, this.thresholdMedium, this.thresholdLow, this.thresholdUnassigned);
+        }
+
+        return thresholds;
     }
 
     /*
      * For testing
      */
-    void setFindingThresholds(FindingThresholds findingThresholds) {
+    void setFindingThresholds(final FindingThresholds findingThresholds) {
         this.findingThresholds = findingThresholds;
     }
 
     private File getOutputDirectory() {
         if (mavenProject == null) {
             return null;
-        }
-        else {
+        } else {
             return new File(mavenProject.getBuild().getDirectory());
         }
     }
