87c2f4b5a805bfbe861cfc78d61112c06a71aa2c
Thomas Hucke <thomas.hucke@soprasteria.com>
2025-05-15 12:09:26 +0200

    chore: working on suppression tests

--- src/test/java/io/github/pmckeown/dependencytrack/suppressions/SupprassionsActionTest.java
+++ src/test/java/io/github/pmckeown/dependencytrack/suppressions/SupprassionsActionTest.java
@@ -1,33 +1,31 @@
 package io.github.pmckeown.dependencytrack.suppressions;
 
-import static io.github.pmckeown.dependencytrack.PollingConfig.TimeUnit.MILLIS;
+import static io.github.pmckeown.dependencytrack.ResponseBuilder.aSuccessResponse;
+import static io.github.pmckeown.dependencytrack.finding.AnalysisBuilder.anAnalysis;
+import static io.github.pmckeown.dependencytrack.finding.ComponentBuilder.aComponent;
+import static io.github.pmckeown.dependencytrack.finding.FindingBuilder.aFinding;
+import static io.github.pmckeown.dependencytrack.finding.FindingListBuilder.aListOfFindings;
+import static io.github.pmckeown.dependencytrack.finding.VulnerabilityBuilder.aVulnerability;
 import static io.github.pmckeown.dependencytrack.project.ProjectBuilder.aProject;
-import static io.github.pmckeown.dependencytrack.upload.UploadBomResponseBuilder.anUploadBomResponse;
+import static io.github.pmckeown.dependencytrack.suppressions.AnalysisBuilder.fixType1Analysis;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 
 import io.github.pmckeown.dependencytrack.CommonConfig;
 import io.github.pmckeown.dependencytrack.DependencyTrackException;
-import io.github.pmckeown.dependencytrack.Poller;
-import io.github.pmckeown.dependencytrack.PollingConfig;
-import io.github.pmckeown.dependencytrack.Response;
+import io.github.pmckeown.dependencytrack.finding.Finding;
 import io.github.pmckeown.dependencytrack.project.Project;
-import io.github.pmckeown.dependencytrack.upload.BomProcessingResponse;
-import io.github.pmckeown.dependencytrack.upload.BomProcessingResponseBuilder;
-import io.github.pmckeown.dependencytrack.upload.UploadBomAction;
-import io.github.pmckeown.dependencytrack.upload.UploadBomRequest;
-import io.github.pmckeown.dependencytrack.upload.UploadBomResponse;
 import io.github.pmckeown.util.Logger;
 import java.util.List;
 import java.util.Optional;
+import kong.unirest.UnirestException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -59,5 +57,14 @@ public class SupprassionsActionTest {
         project = aProject().build();
     }
 
+    @Test
+    public void thatFindingsAreReturned() throws Exception {
+        Project project = aProject().build();
+        doReturn(aSuccessResponse().withBody(fixType1Analysis().build()).build())
+            .when(analysisClient).uploadAnalysis(project.getUuid(), fixType1Analysis().build());
 
+        Optional<UploadAnalysisResponse> returnedAnalysisResponse =
+            analysisClient.uploadAnalysis(project.getUuid(), fixType1Analysis().build()).getBody();
+        assertThat(returnedAnalysisResponse.isPresent(), is(true));
+    }
 }
